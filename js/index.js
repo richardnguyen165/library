587779369f888faf421e6bd2eb179175d198c87c
richard.nguyen1 <richard.nguyen1@ucalgary.ca>
2025-08-03 00:41:35 -0600

    Finished the header

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+
+function Book(title, author, pages, publishYear, genre, read = false) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.publishYear = publishYear;
+  this.genre = genre;
+  this.read = read;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
+
+function displayBooks() {
+  
+}
